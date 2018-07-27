7105adaa92cb95f123d3bc4ae182b77f1ad3983b
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-27 14:17:23 +0530

    Update and rename nnumbers.java to array.java

---
+++ array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class array
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		int sum=0;
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		if(k>=n)
+			System.out.println();
+		else
+		{
+		for(int i=0;i<k;i++)
+		{
+			sum+=a[i];
+		}
+		System.out.println(sum);
+		}
+	}
+}
