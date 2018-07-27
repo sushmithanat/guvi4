286bfd0ae78425017aa11a75d995b5856d0899bc
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-27 12:28:05 +0530

    Update and rename year.java to nhello.java

---
+++ nhello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class nhello
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		long N=sc.nextLong();
+		for(int i=0;i<N;i++)
+			System.out.println("Hello");
+	}
+}
