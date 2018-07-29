36bc30260fc4a3358e231ebcae9f9aab37e2c138
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 16:55:43 +0530

    Update and rename repeated.java to large.java

--- large.java
+++ large.java
@@ -5,25 +5,31 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class repeated
+class large
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int k=sc.nextInt();
 		int[] a=new int[n];
-		int cnt=0;
-		for(int i=0;i<n;i++)
+		int i=0;
+		for(i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
-		for(int i=0;i<n;i++)
+		for(i=0;i<n;i++)
 		{
-			if(a[i]==k)
-			cnt++;
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(a[j]<a[j+1])
+				{
+					int tmp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=tmp;
+				}
+			}
 		}
-		System.out.println(cnt);
+		System.out.println(a[0]);
 		
 	}
 }
