475c074b42979b1638bddc692cf669fd428e7c19
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 16:34:51 +0530

    Update and rename nsum.java to repeated.java

--- repeated.java
+++ repeated.java
@@ -5,18 +5,25 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class nsum
+class repeated
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int sum=0;
-		for(int i=1;i<=n;i++)
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		int cnt=0;
+		for(int i=0;i<n;i++)
 		{
-			sum=sum+i;
+			a[i]=sc.nextInt();
 		}
-		System.out.println(sum);
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			cnt++;
+		}
+		System.out.println(cnt);
 		
 	}
 }
