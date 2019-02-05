4762d8612aec53b4d263e52b0d6a95f12578d288
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2019-02-05 10:05:29 +0530

    Create cpangram.java

---
+++ cpangram.java
@@ -0,0 +1,228 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class cpangram
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int cnt=0,n=str.length();
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='a' || str.charAt(i)=='A')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='b' || str.charAt(i)=='B')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='c' || str.charAt(i)=='C')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='d' || str.charAt(i)=='D')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='e' || str.charAt(i)=='E')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='f' || str.charAt(i)=='F')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='g' || str.charAt(i)=='G')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='h' || str.charAt(i)=='H')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='i' || str.charAt(i)=='I')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='j' || str.charAt(i)=='J')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='k' || str.charAt(i)=='K')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='l' || str.charAt(i)=='L')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='m' || str.charAt(i)=='M')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='n' || str.charAt(i)=='N')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='o' || str.charAt(i)=='O')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='p' || str.charAt(i)=='P')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='q' || str.charAt(i)=='Q')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='r' || str.charAt(i)=='R')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='s' || str.charAt(i)=='S')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='t' || str.charAt(i)=='T')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='u' || str.charAt(i)=='U')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='v' || str.charAt(i)=='V')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='w' || str.charAt(i)=='W')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='x' || str.charAt(i)=='X')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='y' || str.charAt(i)=='Y')
+			{
+			cnt++;
+			break;
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(str.charAt(i)=='z' || str.charAt(i)=='Z')
+			{
+			cnt++;
+			break;
+			}
+		}
+		if(cnt==26)
+		System.out.print("yes");
+		else
+		System.out.print("no");
+	}
+}
